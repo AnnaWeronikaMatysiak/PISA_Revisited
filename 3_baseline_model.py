db5ad04757511d8252af5d1234a9a927f589b167
AnnaWeronikaMatysiak <annaweronika.matysiak@gmail.com>
2022-04-01 17:27:12 +0200

    first trials

--- 3_baseline_model.py
+++ 3_baseline_model.py
@@ -4,4 +4,17 @@ Created on Thu Mar 31 15:14:00 2022
 
 @author: Anna
 """
+#%% import packages
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+#%%
+PISA_sample_10=pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_10.csv/")
+
+X=PISA_sample_10.loc[:, PISA_sample_10.columns.drop(['read_score'])]
+y = PISA_sample_10[["read_score"]]
+
+lin_reg= LinearRegression()
+lin_reg.fit(X, y)
+
 
