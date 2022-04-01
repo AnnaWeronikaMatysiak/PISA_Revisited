024a600fc4bd9d1183feaed9da62a1dcbd9ed2fe
j-mehler <89975174+j-mehler@users.noreply.github.com>
2022-04-01 17:06:03 +0200

    reorganizing (renaming already in first doc) and normalization (new)

--- 2_preprocessing.py
+++ 2_preprocessing.py
@@ -6,6 +6,13 @@ Created on Tue Mar 29 15:14:57 2022
 @author: Jo
 """
 
+#%%
+
+# set working directory
+import os
+os.getcwd()
+os.chdir('/Volumes/GoogleDrive/My Drive/PISA_Revisited/')
+
 #%% import common packages
 
 import numpy as np
@@ -40,18 +47,30 @@ PISA_plot_sample.head()
 PISA_plot_sample.hist(bins=50, figsize=(20,15))
 plt.show()
 
-#renaming relevant columns
-PISA_raw.rename(columns = {'PV1READ':'read_score', 'ST004D01T':'female'}, inplace = True)
+# exploring the dependent variable "reading skills"
+PISA_sample_1000.read_score.mean()
+PISA_sample_1000[["read_score", "female"]].groupby("female").mean()
 
-#exploring our dependent variable
-PISA_raw.read_score.mean()
-PISA_raw[["read_score", "female"]].groupby("female").mean()
-
-PISA_raw.hist(column='read_score',bins=50)
+PISA_sample_1000.hist(column='read_score',bins=50)
 plt.axvline(x=456.1, color='red', linestyle='--')
 plt.show()
 
-#%% normalize features
+#%% Do something with NA's...
+
+
+#%% normalize all features and build a pipeline for preprocessing 
+# the numerical attributes (still check out what it does exactly)
+
+#from sklearn.pipeline import Pipeline
+#from sklearn.preprocessing import StandardScaler
+
+#num_pipeline = Pipeline([
+        #('imputer', SimpleImputer(strategy="median")), # what's this?
+        #('attribs_adder', CombinedAttributesAdder()), #  what's this?
+        #('std_scaler', StandardScaler()),
+    #])
+
+#data_transformed = num_pipeline.fit_transform("numerical_data")
 
 
 
