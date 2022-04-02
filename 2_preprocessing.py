33d54050f652c06c49519b1be4189ce1b5cdff8d
j-mehler <89975174+j-mehler@users.noreply.github.com>
2022-04-02 12:35:23 +0200

    Update 2_preprocessing.py

--- 2_preprocessing.py
+++ 2_preprocessing.py
@@ -36,10 +36,10 @@ PISA_sample_10.info()
 PISA_sample_10.describe()
 
 # plot variables (not really possible with our over 1000 variables...)
-#PISA_sample_100.hist(bins=50, figsize=(20,15))
-#plt.show()
+# PISA_sample_100.hist(bins=50, figsize=(20,15))
+# plt.show()
 
-# --> select random features
+# --> plot variables: select random features
 PISA_plot_sample = PISA_sample_100.sample(n=10,axis='columns')
 PISA_plot_sample.head()
 
@@ -55,7 +55,19 @@ PISA_sample_1000.hist(column='read_score',bins=50)
 plt.axvline(x=456.1, color='red', linestyle='--')
 plt.show()
 
-#%% Do something with NA's...
+
+
+#%% Data Types
+
+
+
+#%% Do something with NA's... -> or is this the 
+
+# find how many NA's in each row, develop a strategy to deal with them
+
+
+
+#%% any other preprocessing (pattern missingness? etc.)
 
 
 
@@ -69,7 +81,7 @@ plt.show()
 # num_pipeline = Pipeline([
         #('imputer', SimpleImputer(strategy="median")), # what's this?
         #('attribs_adder', CombinedAttributesAdder()), #  what's this?
-        #('std_scaler', StandardScaler()),
+        #('std_scaler', StandardScaler()),# this is our normalizer
     #])
 
 # data_transformed = num_pipeline.fit_transform("numerical_data")
