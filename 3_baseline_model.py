055a43db08cb617226427294af57cdbc0e7653b6
AnnaWeronikaMatysiak <annaweronika.matysiak@gmail.com>
2022-04-07 18:39:08 +0200

    chnages to base line and new tasks

--- 3_baseline_model.py
+++ 3_baseline_model.py
@@ -7,21 +7,52 @@ Created on Thu Mar 31 15:14:00 2022
 
 #%% import packages
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 
 #%% call setup file
-import runpy
-runpy.run_path(path_name = '/My Drive/PISA_Revisited/0_setup.py')
-PISA_sample_10=pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_10.csv/")
+#import runpy
+#runpy.run_path(path_name = '/My Drive/PISA_Revisited/0_setup.py')
+PISA_sample_100=pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_100.csv/")
+PISA_sample_100 = PISA_sample_100.columns.drop(["VER_DAT", "CNT"])
 
 # imports sys, sklearn, numpy, os, matplotlib, pathlib
 # checks versions, sets wd, sets random.seed 42, specifies plots
 # defines function save_fig()
 
-#%% dependent and independent variables
-X=PISA_sample_10.loc[:, PISA_sample_10.columns.drop(['read_score'])]
-y = PISA_sample_10[["read_score"]]
+
+#%% preceding steps to enable us run these models (work in progress)
+
+#we should make secision on NAs
+#PISA_sample_100=PISA_sample_100.fillna(value=PISA_sample_100.mean())
+
+#we need to use either LabelEncoder or OneHot thingy
+#OneHotEncoder().fit_transform(PISA_sample_10)
+
+
+#from sklearn.preprocessing import LabelEncoder
+#def Encoder(PISA_sample_100):
+#          columnsToEncode = list(PISA_sample_100.select_dtypes(include=['category','object']))
+#          le = LabelEncoder()
+#          for feature in columnsToEncode:
+#              try:
+#                  PISA_sample_100[feature] = le.fit_transform(PISA_sample_100[feature])
+#              except:
+#                  print('Error encoding '+feature)
+#          return PISA_sample_100
+
+#Encoder(PISA_sample_100)
+      
+
+#%%
+
+X=PISA_sample_100.loc[:, PISA_sample_100.columns.drop(['read_score'])]
+y = PISA_sample_100[["read_score"]]
+
+
+
 
 #%% linear regression
 lin_reg= LinearRegression()
@@ -43,6 +74,8 @@ lin_reg = LinearRegression()
 lin_reg.fit(X_poly, y)
 lin_reg.intercept_, lin_reg.coef_
 lin_reg.predict_
+
+#%% cost funtions
 
 #%% plots
 
