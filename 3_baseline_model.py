666b4645d327824162be982b3dad3729b73767dc
AnnaWeronikaMatysiak <annaweronika.matysiak@gmail.com>
2022-04-10 18:40:32 +0200

    added ridge, plus fixed a few lines

--- 3_baseline_model.py
+++ 3_baseline_model.py
@@ -15,12 +15,13 @@ from sklearn.preprocessing import PolynomialFeatures
 #%% call setup file
 #import runpy
 #runpy.run_path(path_name = '/My Drive/PISA_Revisited/0_setup.py')
-PISA_sample_100=pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_100.csv/")
-PISA_sample_100 = PISA_sample_100.columns.drop(["VER_DAT", "CNT"])
+PISA_sample_100 = pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_100.csv")
+PISA_sample_100 = PISA_sample_100.columns.drop(["VER_DAT", "CNT", "CYC", "STRATUM"])
 
 # imports sys, sklearn, numpy, os, matplotlib, pathlib
 # checks versions, sets wd, sets random.seed 42, specifies plots
 # defines function save_fig()
+
 
 
 #%% preceding steps to enable us run these models (work in progress)
@@ -47,40 +48,52 @@ PISA_sample_100 = PISA_sample_100.columns.drop(["VER_DAT", "CNT"])
       
 
 #%%
-
-X=PISA_sample_100.loc[:, PISA_sample_100.columns.drop(['read_score'])]
-y = PISA_sample_100[["read_score"]]
+X_tain=PISA_sample_100[:,:'VER_DAT', 'CNT', 'CYC', 'STRATUM']
+#y = PISA_sample_100[['read_score']]
+y_train=PISA_sample_100[:,'read_score']
 
 #%% linear regression
 lin_reg= LinearRegression()
-lin_reg.fit(X, y)   
+lin_reg.fit(X_train, y_train)   
 
 lin_reg.coef_
 lin_reg.intercept_
-lin_reg.predict(X)
+predicted=lin_reg.predict(X_train)
+predicted
+
+#%%plots
+plt.scatter(X_train, y_train, color="black")
+plt.plot(X_train, predicted, color="blue", linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
 
 #%% RMSE
 
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
-mse = mean_squared_error(y_true, y_predicted, squared=False)
-rmse= np.sqrt(metrics.mean_squared_error(y_true, y_predicted)
+mse = mean_squared_error(y_train, predicted, squared=False)
+rmse= np.sqrt(metrics.mean_squared_error(y_train, predicted)
 
 #%% polynomial regression
+
 poly_features = PolynomialFeatures(degree=2, include_bias=False)
 X_poly = poly_features.fit_transform(X)
 X[0]
 
 lin_reg_pol = LinearRegression()
-lin_reg_pol.fit(X_poly, y)
+lin_reg_pol.fit(X_poly, y_train)
 lin_reg_pol.intercept_, lin_reg.coef_
-lin_reg_pol.predict(X)
+lin_reg_pol.predict(X_poly)
 
-#%% cost funtions
-
-#%% plots
-
-#%% validation and evaluation measures
+#%% ridge regression
+rr = Ridge(alpha=0.01)
+rr.fit(X_train, y_train) 
+pred_train_rr= rr.predict(X_train)
+print(np.sqrt(mean_squared_error(y_train,pred_train_rr)))
+print(r2_score(y_train, pred_train_rr))
 
 
