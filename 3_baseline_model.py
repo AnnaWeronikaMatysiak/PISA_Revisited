b12e29e53cff85b4bd3b3d0acb42824c266b87de
AnnaWeronikaMatysiak <annaweronika.matysiak@gmail.com>
2022-04-11 12:53:27 +0200

    added ridge model, corrected metrics, added to do

--- 3_baseline_model.py
+++ 3_baseline_model.py
@@ -5,52 +5,55 @@ Created on Thu Mar 31 15:14:00 2022
 @author: Anna
 """
 
+"""
+TO DO:
+- check the evaluation metrics, 
+- check if that's what Eric recommended 
+- add plots
+- find out at which point we need to use test and when shall we compare between the models
+- are we able/allowed to compare them now already?
+- at the end, change PISA_sample_100 to the final sample we are using'
+"""
+
 #%% import packages
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import Ridge
 
 #%% call setup file
 #import runpy
 #runpy.run_path(path_name = '/My Drive/PISA_Revisited/0_setup.py')
 PISA_sample_100 = pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_100.csv")
-PISA_sample_100 = PISA_sample_100.columns.drop(["VER_DAT", "CNT", "CYC", "STRATUM"])
+PISA_sample_100 = PISA_sample_100.drop(columns=["VER_DAT", "CNT", "CYC", "STRATUM"])
 
-# imports sys, sklearn, numpy, os, matplotlib, pathlib
-# checks versions, sets wd, sets random.seed 42, specifies plots
-# defines function save_fig()
-
-
-
-#%% preceding steps to enable us run these models (work in progress)
-
-#we should make secision on NAs
-#PISA_sample_100=PISA_sample_100.fillna(value=PISA_sample_100.mean())
-
-#we need to use either LabelEncoder or OneHot thingy
-#OneHotEncoder().fit_transform(PISA_sample_10)
-
-
-#from sklearn.preprocessing import LabelEncoder
-#def Encoder(PISA_sample_100):
-#          columnsToEncode = list(PISA_sample_100.select_dtypes(include=['category','object']))
-#          le = LabelEncoder()
-#          for feature in columnsToEncode:
-#              try:
-#                  PISA_sample_100[feature] = le.fit_transform(PISA_sample_100[feature])
-#              except:
-#                  print('Error encoding '+feature)
-#          return PISA_sample_100
-
-#Encoder(PISA_sample_100)
-      
 
 #%%
-X_tain=PISA_sample_100[:,:'VER_DAT', 'CNT', 'CYC', 'STRATUM']
-#y = PISA_sample_100[['read_score']]
-y_train=PISA_sample_100[:,'read_score']
+PISA_sample_100 = pd.read_csv("/My Drive/PISA_Revisited/data/PISA_sample_100.csv")
+#utill we drop these variables from the whole data set,
+# I manualy exclude them from sample
+PISA_sample_100 = PISA_sample_100.drop(columns=["VER_DAT", "CNT", "CYC", "STRATUM"])
+
+X_train=PISA_sample_100.drop(columns=["read_score"])
+y_train=PISA_sample_100["read_score"]
+
+#becuase y is an array, I change it back to data frame
+y_train=y_train.to_frame()
+
+
+#for test untill we have clean data I replace the data columns
+# with NAs witht the colum means. This does not work for "test" column, so I drop it. 
+#By the way, what is that column about?
+y_train=y_train.apply(lambda x: x.fillna(x.mean()))
+X_train=X_train.apply(lambda x: x.fillna(x.mean())) 
+X_train=X_train.drop(columns=["test"])
+
+#code to check if any of the columns have NAs:
+#y_train.isnull().any()
 
 #%% linear regression
 lin_reg= LinearRegression()
@@ -59,41 +62,58 @@ lin_reg.fit(X_train, y_train)
 lin_reg.coef_
 lin_reg.intercept_
 predicted=lin_reg.predict(X_train)
-predicted
+predicted.heac()
+
+#%% evaluation 
+"""questions to Eric 
+- are we supposed to calculate it now (at this stage) on train or test tests?
+- why our mse and rmse are the same?"""
+mse = mean_squared_error(y_train, predicted, squared=False)
+rmse= np.sqrt(mean_squared_error(y_train, predicted))
+mae=mean_absolute_error(y_train, predicted)
+
+print(mse)
+print(rmse)
+print(mae)
 
 #%%plots
-plt.scatter(X_train, y_train, color="black")
 plt.plot(X_train, predicted, color="blue", linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
-
-#%% RMSE
-
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-
-mse = mean_squared_error(y_train, predicted, squared=False)
-rmse= np.sqrt(metrics.mean_squared_error(y_train, predicted)
 
 #%% polynomial regression
 
 poly_features = PolynomialFeatures(degree=2, include_bias=False)
-X_poly = poly_features.fit_transform(X)
-X[0]
+X_poly = poly_features.fit_transform(X_train)
+X_train[0]
 
 lin_reg_pol = LinearRegression()
 lin_reg_pol.fit(X_poly, y_train)
 lin_reg_pol.intercept_, lin_reg.coef_
-lin_reg_pol.predict(X_poly)
+predicted_poly=lin_reg_pol.predict(X_poly)
+predicted_poly.head()
+
+#%% evaluation
+#ask Eric as above
+mse_poly = mean_squared_error(y_train, predicted_poly, squared=False)
+rmse_poly= np.sqrt(mean_squared_error(y_train, predicted_poly))
+mae_poly=mean_absolute_error(y_train, predicted_poly)
+
+print(mse_poly)
+print(rmse_poly)
+print(mae_poly)
 
 #%% ridge regression
-rr = Ridge(alpha=0.01)
-rr.fit(X_train, y_train) 
-pred_train_rr= rr.predict(X_train)
-print(np.sqrt(mean_squared_error(y_train,pred_train_rr)))
-print(r2_score(y_train, pred_train_rr))
+ridge_reg = Ridge(alpha=1, solver="cholesky")
+ridge_reg.fit(X_train, y_train)
+predicted_ridge=ridge_reg.predict(X_train)
 
+#%% evaluation 
+#ask Eric - do we need more or only these measures
+
+mse_ridge = mean_squared_error(y_train, predicted_ridge, squared=False)
+rmse_ridge= np.sqrt(mean_squared_error(y_train, predicted_ridge))
+mae_ridge=mean_absolute_error(y_train, predicted_ridge)
+
+print(mse_ridge)
+print(rmse_ridge)
+print(mae_ridge)
 
