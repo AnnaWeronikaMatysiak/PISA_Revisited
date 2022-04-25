f6618ca34bd1429c98c43bd053a6bfb33c12f8b4
AnnaWeronikaMatysiak <annaweronika.matysiak@gmail.com>
2022-04-25 20:37:10 +0200

    split according to the lab and ready for new dataset

--- 3_baseline_models.py
+++ 3_baseline_models.py
@@ -7,11 +7,14 @@ Created on Thu Mar 31 15:14:00 2022
 
 """
 TO DO:
-- next stage: plots
-- next stage: visualisation of error
-- next stage: use whole data set
-- next stage:predictions' description an ordering them
+- change datasets to the "preprocessed 1000"
+- visualisation of error
+- predictions' description an ordering them
 """
+
+#%% call setup file
+import runpy
+runpy.run_path(path_name = '/0_setup.py')
 
 #%% necessary packages
 from sklearn.linear_model import LinearRegression
@@ -20,10 +23,6 @@ import numpy as np
 #import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
-
-#%% call setup file
-import runpy
-runpy.run_path(path_name = '/My Drive/PISA_Revisited/0_setup.py')
 
 #%% read in data
 
@@ -61,7 +60,4 @@ mae= mean_absolute_error(y_validation, y_predicted)
 print('MSE_linear: ',mse)
 print('RMSE_linear: ',rmse)
 print('MSE_linear: ', mae)
-
-
-
 
---
+++ old_split.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 20:28:33 2022
+
+@author: Anna
+"""
+
