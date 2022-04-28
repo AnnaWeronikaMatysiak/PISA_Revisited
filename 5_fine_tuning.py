b6eda1537e8362e32236f9db82f7f27e77530ff7
j-mehler <89975174+j-mehler@users.noreply.github.com>
2022-04-28 11:13:17 +0200

    sections

--- 5_fine_tuning.py
+++ 5_fine_tuning.py
@@ -6,3 +6,27 @@ Created on Wed Apr 27 17:42:21 2022
 
 """
 
+#%% model 1 poly-ridge degree 2
+
+
+section 1
+
+
+
+#%% model 2 poly-ridge degree 3
+
+
+section 2
+
+
+
+#%% model 3 RandomForest
+
+
+section 3
+
+
+#%% model 4 ExtraTrees
+
+
+section 4
