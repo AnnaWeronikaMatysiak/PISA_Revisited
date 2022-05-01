eae7cc7dc290969d4523e48f7bf8f424e1d243c7
AnnaWeronikaMatysiak <annaweronika.matysiak@gmail.com>
2022-05-02 01:09:47 +0200

    Update 4a_modeling_and_tuning_ridge.py

--- 4a_modeling_and_tuning_ridge.py
+++ 4a_modeling_and_tuning_ridge.py
@@ -40,7 +40,7 @@ runpy.run_path(path_name = '/0_setup.py')
 # defines function save_fig()
 
 #%% read in data
-#X_test = pd.read_csv("/My Drive/PISA_Revisited/data/X_test.csv") 
+X_test = pd.read_csv("/My Drive/PISA_Revisited/data/X_test.csv") 
 #y_test = pd.read_csv("/My Drive/PISA_Revisited/data/y_test.csv")
 
 X_train = pd.read_csv("data/X_train.csv")
@@ -58,11 +58,10 @@ def drop_first_entry(df):
     df.drop(df.columns[[0]], axis = 1, inplace = True)
 
 drop_first_entry(X_train)
-drop_first_entry(y_train)
 drop_first_entry(X_val_1)
-drop_first_entry(y_val_1)
+drop_first_entry(X_test)
 drop_first_entry(X_val_2)
-drop_first_entry(y_val_2)
+
 
 #%% initial ridge regression - mid term report
 #training
@@ -78,9 +77,9 @@ rmse_ridge= np.sqrt(mean_squared_error(y_val_1, predicted_ridge))
 mae_ridge=mean_absolute_error(y_val_1, predicted_ridge)
 r_2_ridge=r2_score(y_val_1, predicted_ridge)
 
-print('RMSE_ridge: ',rmse_ridge) #result: RMSE=68.51031872157898
-print('MAE_linear:', mae_ridge) # result: MAE=54.398277624619126
-print('R_2_linear:', r_2_ridge) #result: R_2=0.5922037262775235
+print('RMSE_ridge: ',rmse_ridge) #result: RMSE=68.5100950967124
+print('MAE_linear:', mae_ridge) # result: MAE=54.39827907320501
+print('R_2_linear:', r_2_ridge) #result: R_2=0.5922063884528036
 
 #%% simple ridge regression
 #trianing
@@ -92,44 +91,53 @@ predicted_ridge=ridge_model.predict(X_val_1)
 
 #evaluation
 rmse_ridge= np.sqrt(mean_squared_error(y_val_1, predicted_ridge))
-print('RMSE_ridge: ',rmse_ridge) #result: 68.52007866924971
+print('RMSE_ridge: ',rmse_ridge) #result: 68.51986262316217
 
 #saving model
-joblib.dump(ridge_model, "/models/ridge.pkl")
+joblib.dump(ridge_model, "models/ridge.pkl")
 
 #cross-validation on the ridge
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
 scores = cross_val_score(ridge, X_val_1, y_val_1, scoring='neg_mean_squared_error', cv=cv, n_jobs=-1)
 
 print('RMSE_ridge_after_cross_validation: ',np.mean(np.sqrt(np.abs(scores))))
-#result: 69.1597380704831
+#result: 69.15260004052853
 
 #%% fine-tuning
 
-pipe=make_pipeline(Ridge())
+model=make_pipeline(Ridge())
 
 #putting together a parameter grid to search over using grid search
+#alphas based on the first tuning when the alpha was 6
 params={
     'ridge__fit_intercept':[True,False],
-    'ridge__alpha':[0.001,0.01,0.1, 1,2,3,4,5,6,7,8,9,10,100],
+    'ridge__alpha':[4,5,6,7,8],
     'ridge__solver':[ 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag',
 'saga']
 }
-#setting up the grid search
-gs=GridSearchCV(pipe,params,n_jobs=-1,cv=cv, scoring="neg_mean_squared_error")
+#grid search
+gs=GridSearchCV(model,params,n_jobs=-1,cv=cv, scoring="neg_mean_squared_error")
 #fitting gs to data
-results=gs.fit(X_val_2, y_val_2)
+gs.fit(X_val_2, y_val_2)
 
-print(gs.best_estimator_.get_params()["ridge"])
-print('Best Score: %s' % np.sqrt(-results.best_score_))
-print('Best Hyperparameters: %s' % results.best_params_)
+#print(gs.best_estimator_.get_params()["ridge"])
+print('Best Score: %s' % np.sqrt(-gs.best_score_))
+print('Best Hyperparameters: %s' % gs.best_params_)
 
 #results:
+#this was on the train data, so probaby overfitted
 #Best Score: 48.089040334701856
 #Best Hyperparameters: {'ridge__alpha': 5, 'ridge__fit_intercept': True, 'ridge__solver': 'cholesky'}
 
+#this is a result on the correct val_2 data
+#Ridge(alpha=5, solver='sag')
+#Best Score: 67.9992874603911
+#Best Hyperparameters: {'ridge__alpha': 5, 'ridge__fit_intercept': True, 'ridge__solver': 'sag'}
 
-#save the model
+#%%save the model
+final_ridge_model= gs.best_estimator_
+joblib.dump(final_ridge_model, "models/final_ridge_model.pkl")
+
 #%% feature transformation to degree=2
 poly_features = PolynomialFeatures(degree=2, include_bias=False)
 X_poly_2 = poly_features.fit_transform(X_train)
@@ -137,7 +145,7 @@ X_poly_2=pd.DataFrame(X_poly_2)
 X_val_1_poly=poly_features.fit_transform(X_val_1)
 X_val_1_poly=pd.DataFrame(X_val_1_poly)
 X_val_2_poly=poly_features.fit_transform(X_val_2)
-
+X_val_2_poly=pd.DataFrame(X_val_2_poly)
 #%% polynomial regression with ridge regularisation - degree 2
 #training model
 poly_reg_w_ridge = Ridge()
@@ -154,16 +162,28 @@ print('RMSE_ridge_poly_2: ',rmse_poly2)
 print ('R2_ridge_poly2:', r2_poly2)
 
 # saving the model
-joblib.dump(poly_reg_w_ridge, "/models/poly_reg_w_ridge.pkl")
+joblib.dump(poly_reg_w_ridge, "models/poly_reg_w_ridge.pkl")
 
 #%% fine-tuning polynomial model with ridge 
 
-#fitting fine-tuning to training data on polynomialy transfomred data
-results_2=gs.fit(X_val_2_poly, y_val_2)
+model2=make_pipeline(Ridge())
 
-# results of fine-tuning for polynomialy transformed data
-print(gs.best_estimator_.get_params()["ridge"])
-print('Best Score: %s' % np.sqrt(-results_2.best_score_))
-print('Best Hyperparameters: %s' % results_2.best_params_)
+#putting together a parameter grid to search over using grid search
+params={
+    'ridge__fit_intercept':[True,False],
+    'ridge__alpha':[4,5,6,7,8],
+    'ridge__solver':[ 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag',
+'saga']
+}
+#setting up the grid search
+gs2=GridSearchCV(model2,params,n_jobs=-1,cv=cv, scoring="neg_mean_squared_error")
+#fitting gs to data
+gs2.fit(X_val_2_poly, y_val_2)
 
+print(gs2.best_estimator_.get_params()["ridge"])
+print('Best Score: %s' % np.sqrt(-gs2.best_score_))
+print('Best Hyperparameters: %s' % gs2.best_params_)
 
+#%%save the model
+final_poly_ridge= gs2.best_estimator_
+joblib.dump(final_poly_ridge, "models/final_poly_ridge_tuned.pkl")
